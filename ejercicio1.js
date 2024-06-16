43b0c714904a300c01e4753df0ef5100eb69cf15
Gabriel Martinez <gaabi26092005@gmail.com>
2024-06-16 19:30:12 -0300

    Ejercicio1.js completado

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+// Uso de for...of y Arrays: 
+// Escribe una función que reciba un array de números y retorne un nuevo array con cada número elevado al cuadrado. 
+// Utiliza un bucle for...of para iterar sobre los elementos del array.
+
+function elevar_al_cuadrado_un_array(lista){
+    lista_modificada = []
+    for (const item of lista) {
+        lista_modificada.push(item * item)        
+    }
+    return lista_modificada
+}
+
+
+array = [2, 4, 6]
+array_cuadrado = elevar_al_cuadrado_un_array(array)
+console.log(array_cuadrado)
+
+
